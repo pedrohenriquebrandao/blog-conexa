8982d6777d2c7dba71eee030702e4b7b9ff028c3
Pedro Brandão <phbrandao@outlook.com>
2022-08-10 12:06:24 -0300

    Remoção script desnecessário

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -22,7 +22,6 @@
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,200;0,300;0,400;1,300&display=swap" rel="stylesheet">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" ></script>
     <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
